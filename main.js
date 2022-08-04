ac8cd729d09f823e7322a2fb9165dc7845cfff2b
jatinrao241 <jatinrao137@gmail.com>
2022-08-04 23:11:20 +0530

    first commit
    ...

---
+++ main.js
@@ -0,0 +1,22 @@
+<script nonce="7HifP+WNyBiH5MOuTdFPylnDev4=" type="e08b97a02cfa8c34f70fa8ce-text/javascript">
+
+function(w, d) {
+    (w.requestAnimationFrame || w.mozRequestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.oRequestAnimationFrame || function (f) {
+        var e = 'load',
+            n, h = w[n = 'addEventListener'] || w[n = 'attachEvent'] && (e = 'on' + e);
+        h ? w[n](e, f) : setTimeout(f, 17)
+    })(function () {
+        for (var h = d.getElementsByTagName('link'), i = h && h.length, l, c; 0 < i--;)(l = h[i]).rel == 'preload' && l.type == 'text/css' && (c = d.createElement('link'), c.rel = 'stylesheet', c.type = l.type, c.href = l.href, l.parentNode.insertBefore(c, l))
+    })
+} (window, document));
+var nfcShareData = {
+    records: [{
+        recordType: "url",
+        data: "https://dizitalcard.in/anunagarstudio/",
+    }]
+};
+var webShareData = {
+    title: 'Anu Nagar Studio',
+    url: 'https://dizitalcard.in/anunagarstudio/'
+}
+</script>
